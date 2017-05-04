842c1b02720b1c516bc4c616a04fb518adcfae4a
Mobile\JustinMorgan <jumorgan@pdx.edu>
2017-05-04 10:56:27 -0700

    Fixed forrest janky code

--- VishnuSocket/VishnuMain/SettingsLibrary.cs
+++ VishnuSocket/VishnuMain/SettingsLibrary.cs
@@ -21,17 +21,17 @@ namespace VishnuMain
         static string _invPathString;
 
         //encapsulated stubs begin here
-        public static double TrayOrigin2CenterX { get => _trayOrigin2CenterX; set => _trayOrigin2CenterX = value; }
-        public static double TrayOrigin2CenterY { get => _trayOrigin2CenterY; set => _trayOrigin2CenterY = value; }
-        public static double TrayCenter2CenterRow { get => _trayCenter2CenterRow; set => _trayCenter2CenterRow = value; }
-        public static double TrayCenter2CenterCol { get => _trayCenter2CenterCol; set => _trayCenter2CenterCol = value; }
+        public static double TrayOrigin2CenterX { get { return _trayOrigin2CenterX; } set { _trayOrigin2CenterX = value; } }
+        public static double TrayOrigin2CenterY { get { return _trayOrigin2CenterY; } set { _trayOrigin2CenterY = value; } }
+        public static double TrayCenter2CenterRow { get { return _trayCenter2CenterRow; } set { _trayCenter2CenterRow = value; } }
+        public static double TrayCenter2CenterCol { get { return _trayCenter2CenterCol; } set { _trayCenter2CenterCol = value; } }
 
-        public static double TrayHeight { get => _trayHeight; set => _trayHeight = value; }
-        public static int TrayLength { get => _trayLength; set => _trayLength = value; }
-        public static int TrayWidth { get => _trayWidth; set => _trayWidth = value; }
-        public static int TrayStack { get => _trayStack; set => _trayStack = value; }
+        public static double TrayHeight { get { return _trayHeight; } set { _trayHeight = value; } }
+        public static int TrayLength { get { return _trayLength; }  set { _trayLength = value; } }
+        public static int TrayWidth { get { return _trayWidth; } set { _trayWidth = value; } }
+        public static int TrayStack { get { return _trayStack; } set { _trayStack = value; } }
         
-        public static string InvPathString { get => _invPathString; set => _invPathString = value; }
+        public static string InvPathString { get { return _invPathString; } set { _invPathString = value; } }
         
     }
 }
