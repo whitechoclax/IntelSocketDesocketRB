accf756fd16e184bbb8f4ded4d05d765f9c62e0b
Forrest Kaminski <fgk55555@gmail.com>
2017-05-16 21:52:37 -0700

    Added persistant memory

--- CPUGrabber/CPUGrabber.ino
+++ CPUGrabber/CPUGrabber.ino
@@ -5,36 +5,43 @@
    using A4988 Stepper motor Drivers
 */
 
+#include <Servo.h>
+#include <EEPROMex.h>
+Servo effector;
+
 //Millimeter to step mappings
 # define RAD 100
 # define THETA 2 //to one degree
 # define Z 100
 
-//power settings: .5 A , 12v 
+//power settings: .5 A , 24v 
 //Motor Mappings
 #define RADMOTOR 0
 #define THETAMOTOR 2
 #define ZMOTOR 4
-#define ANGLEMOTOR 3
+#define ANGLEMOTOR 6
 #define RIGHT true
 #define LEFT false
 #define OUT true
 #define IN false
 //Grabber solenoid and Servo pins
-#define GRABBER 26
+#define GRABBER 29
 #define SERVO 8
-
+#define RADMEM 0
+#define THETAMEM 4
+#define ZMEM 8
+#define ENDMEM 12
 
 boolean DEBUG = false; 
 
 //Current position
 double xpos = 0;
 double ypos = 150;
-int zpos = 200;
-double angle = 0; //Angle for end effector
+double zpos = EEPROM.readDouble(ZMEM);
+double angle = EEPROM.readDouble(ENDMEM); //Angle for end effector
 
-double theta = 0; //angle of robot
-double radius = 150; //extension of arm
+double theta = EEPROM.readDouble(THETAMEM); //angle of robot
+double radius = EEPROM.readDouble(RADMEM); //extension of arm
 
 double thetaNew = 0;
 double radiusNew = 0;
@@ -62,7 +69,7 @@ boolean stringComplete = false;
 
 void setup() {
   Serial.begin(115200);
-
+  MapCoordinates(true);
   //Setup for pinouts
   for (int i = 0; i < 6; ++i) {
     pinMode(Enable[i], OUTPUT);
@@ -72,9 +79,14 @@ void setup() {
     pinMode(Dir[i], OUTPUT);
     digitalWrite(Dir[i], LOW);
   }
+  pinMode(31, OUTPUT);
+  pinMode(33, OUTPUT);
+  digitalWrite(31, HIGH);
+  digitalWrite(33, LOW);
   pinMode(GRABBER, OUTPUT);
   digitalWrite(GRABBER, LOW);
   Serial.println("MAINROBOTARM");
+  effector.attach(ANGLEMOTOR);
 }
 
 void loop() {
